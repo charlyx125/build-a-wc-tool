f17f58322cda4924bcc8979644074c02fc68a5e2
joyce-adserve <joyce.chong@adserve.com>
2026-07-13 16:43:27 +0100

    Add functions to return number of bytes, lines and words in a given file

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import os
+import sys
+
+
+def get_bytes(file_path):
+    try:
+        return os.path.getsize(file_path)
+    except Exception:
+        print(f"wc: {file_path}: No such file or directory")
+        return None
+
+
+def get_lines(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as fp:
+            return sum(1 for line in fp)
+    except Exception:
+        print(f"wc: {file_path}: No such file or directory")
+        return None
+
+
+def get_words(file_path):
+    try:
+        count = 0
+        with open(file_path, "r", encoding="utf-8") as fp:
+            data = fp.read()
+            words = data.split()
+            count += len(words)
+            return count
+    except Exception:
+        print(f"wc: {file_path}: No such file or directory")
+        return None
+
+
 def main():
-    print("Hello, World!")
+    result = get_words("test.txt")
+    if result is not None:
+        print(result)
+    print(sys.argv)
 
 
 if __name__ == "__main__":
